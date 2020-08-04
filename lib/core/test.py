bc21c61cbdd9887052ee21d05f8db72283f358f5
HEIMaxER <maxime.plocharski@gmail.com>
2020-08-04 19:10:34 +0200

    bug fix

--- lib/core/test.py
+++ lib/core/test.py
@@ -452,7 +452,7 @@ def box_results_with_nms_limit_and_openset_threshold(scores, boxes, threshold): 
             )
         cls_boxes.append(nms_dets)
     else:
-        cls_boxes.append(np.empty(0)
+        cls_boxes.append(np.empty(0))
     # Limit to max_per_image detections **over all classes**
     if cfg.TEST.DETECTIONS_PER_IM > 0:
         image_scores = np.hstack(
