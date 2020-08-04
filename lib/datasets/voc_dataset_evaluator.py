663b4225a40f64097658599ba3a3df6fdba96b25
HEIMaxER <maxime.plocharski@gmail.com>
2020-08-04 23:52:42 +0200

    debug print

--- lib/datasets/voc_dataset_evaluator.py
+++ lib/datasets/voc_dataset_evaluator.py
@@ -47,6 +47,7 @@ def evaluate_boxes(
     use_matlab=False, seed=None, unkwn_nbr=None, mode=None
 ):
     salt = '_{}'.format(str(uuid.uuid4())) if use_salt else ''
+    print(seed, unkwn_nbr, mode)
     filenames = _write_voc_results_files(json_dataset, all_boxes, salt, seed, unkwn_nbr, mode)
     if test_corloc:
         _eval_discovery(json_dataset, salt, output_dir)
