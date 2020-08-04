56e517c05b220c940509f9221c6ebd34d8f4bc23
HEIMaxER <maxime.plocharski@gmail.com>
2020-08-04 16:00:57 +0200

    debug prints

--- lib/core/test.py
+++ lib/core/test.py
@@ -425,7 +425,7 @@ def box_results_with_nms_limit_and_openset_threshold(scores, boxes, threshold): 
 
     os_scores = []
     os_boxes = []
-    print('min : ', min(scores), 'max : ', max(scores))
+    print('min : ', min([min(scores[i]) for i in range(len(scores))]), 'max : ', max([max(scores[i]) for i in range(len(scores))]))
     for i in range(len(scores)):                 #looking for new objects
         if all(scores[i]) < threshold:
             os_scores.append(1-max(scores[i]))
