6923b3d2d66d9ad2d78920992591aa99119ff229
HEIMaxER <maxime.plocharski@gmail.com>
2020-08-04 16:27:10 +0200

    debug prints

--- lib/core/test.py
+++ lib/core/test.py
@@ -419,7 +419,10 @@ def box_results_with_nms_limit_and_openset_threshold(scores, boxes, threshold): 
         cls_boxes[j] = nms_dets
     os_scores = []
     os_boxes = []
-    print('cls_boxes', cls_boxes)
+    print('cls_boxes')
+    print(len(cls_boxes))
+    for j in range(len(cls_boxes)):
+        print(cls_boxes[j].shape)
     for i in range(len(scores)):                 #looking for new objects
         if max(scores[i]) < os_threshold:
             os_scores.append(1-max(scores[i]))
