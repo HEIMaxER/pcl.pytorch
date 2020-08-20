8b20ba5e7c735296f93a203ef5e7bef70e0fb099
HEIMaxER <maxime.plocharski@gmail.com>
2020-08-20 14:44:25 +0200

    fixed bug

--- lib/datasets/voc_eval.py
+++ lib/datasets/voc_eval.py
@@ -229,9 +229,6 @@ def voc_eval(detpath,
     # compute precision recall
     fp = np.cumsum(fp)
     tp = np.cumsum(tp)
-    print('classe : ', classname)
-    print('True positives : ', np.sum(tp), np.sum(tp)/nd)
-    print('False positives : ', np.sum(fp), np.sum(fp)/nd)
     rec = tp / float(npos)
     # avoid divide by zero in case the first detection matches a difficult
     # ground truth
