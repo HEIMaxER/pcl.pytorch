249991878d7b4d151a4196f23f5a83a858304e5c
HEIMaxER <maxime.plocharski@gmail.com>
2020-08-20 18:30:56 +0200

    added print

--- lib/datasets/voc_dataset_evaluator.py
+++ lib/datasets/voc_dataset_evaluator.py
@@ -45,7 +45,6 @@ def eval_classification(json_dataset,
     use_matlab=True, seed=None, unkwn_nbr=None, mode=None, threshold=None):
     salt = '_{}'.format(str(uuid.uuid4())) if use_salt else ''
     filenames = _write_voc_results_classification_files(json_dataset, detected_class_ids, salt, seed=seed, unkwn_nbr=unkwn_nbr, mode=mode)
-    salt = '_{}'.format(str(uuid.uuid4())) if use_salt else ''
     info = voc_info(json_dataset)
     year = info['year']
     anno_path = info['anno_path']
