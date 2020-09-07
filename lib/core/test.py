173e45e37d2d6a0cc7cb67c801a012aceee2f962
HEIMaxER <maxime.plocharski@gmail.com>
2020-09-07 23:38:07 +0200

    removed print

--- lib/core/test.py
+++ lib/core/test.py
@@ -400,7 +400,6 @@ def random_box_results_with_nms_limit(scores, boxes):  # NOTE: support single-ba
         box_nbr = rd.randint(0, 420)
         inds = np.random.choice(scores.shape[0], box_nbr)
         scores_j = rd.rand(box_nbr)
-        print('scores:', type(scores_j), scores_j.shape)
         boxes_j = boxes[inds, :]
         dets_j = np.hstack((boxes_j, scores_j[:, np.newaxis])).astype(np.float32, copy=False)
         if cfg.TEST.SOFT_NMS.ENABLED:
