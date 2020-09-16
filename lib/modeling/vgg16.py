82818e8254e2afb6ce26948a307f59f034a16d0e
HEIMaxER <maxime.plocharski@gmail.com>
2020-09-16 15:11:41 +0200

    bug fix

--- lib/modeling/vgg16.py
+++ lib/modeling/vgg16.py
@@ -204,6 +204,7 @@ class roi_2mlp_head_with_sim(nn.Module):
                 if (feature_ranking[i][N-self.sim_dim:] == feature_ranking[j][N-self.sim_dim:]).all():
                     sim_mat[i][j] = 1
                     sim_mat[j][i] = 1
+        sim_mat.to('cuda')
         return x, sim_mat
 
 
