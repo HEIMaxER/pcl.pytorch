d9613c6122ddc5a856bf156bcf1a254d21b1f50b
HEIMaxER <maxime.plocharski@gmail.com>
2020-09-16 15:00:17 +0200

    bug fix

--- lib/openset/cluster_loss.py
+++ lib/openset/cluster_loss.py
@@ -15,13 +15,13 @@ def BCE_loss(box_cls_scores, sim_mat):
     print("box scores shape : ", box_cls_scores.shape)
     for i in range(M):
         for j in range(M):
-            cls_score_i = box_cls_scores[i]
-            cls_score_j = box_cls_scores[j]
+            cls_score_i = box_cls_scores[i][0, :].resize(-1, 1)
+            cls_score_j = box_cls_scores[j][0, :]
 
             cls_score_i = cls_score_i.clamp(1e-6, 1 - 1e-6)
             cls_score_j = cls_score_j.clamp(1e-6, 1 - 1e-6)
 
-            loss -= sim_mat[i][j] * torch.log(torch.transpose(cls_score_i, 0) * cls_score_j) + (1 - sim_mat[i][j]) * torch.log(1 - torch.transpose(cls_score_i, 0) * cls_score_j)
+            loss -= sim_mat[i][j] * torch.log(cls_score_i * cls_score_j) + (1 - sim_mat[i][j]) * torch.log(1 - torch.transpose(cls_score_i, 0, 0) * cls_score_j)
 
     loss *= (1/M**2)
 
