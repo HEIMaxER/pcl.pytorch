a9e929c5feadc35b1cbfb770afbb8f0261d9ea03
HEIMaxER <maxime.plocharski@gmail.com>
2020-09-16 18:07:59 +0200

    removed print

--- lib/openset/cluster_loss.py
+++ lib/openset/cluster_loss.py
@@ -11,9 +11,5 @@ import numpy as np
 
 def BCE_loss(box_cls_scores, sim_mat):
     t_box_cls_scores = torch.transpose(box_cls_scores, 0, 1)
-    loss_one = torch.log(torch.mm(box_cls_scores, t_box_cls_scores))
-    loss_one = - sim_mat * loss_one
-    print("loss_one shape : ", loss_one.shape)
-    loss_mone = - (1 - sim_mat) * torch.log(1 - torch.mm(box_cls_scores, t_box_cls_scores))
-    loss = loss_one + loss_mone
+    loss = - sim_mat * torch.log(torch.mm(box_cls_scores, t_box_cls_scores)) - (1 - sim_mat) * torch.log(1 - torch.mm(box_cls_scores, t_box_cls_scores))
     return loss.mean()
