c197eb750c7b81481828465ef966cb7c21c9896d
HEIMaxER <maxime.plocharski@gmail.com>
2020-09-16 15:42:31 +0200

    bug fix

--- lib/openset/cluster_loss.py
+++ lib/openset/cluster_loss.py
@@ -16,14 +16,14 @@ def BCE_loss(box_cls_scores, sim_mat):
     print("box scores shape : ", box_cls_scores.shape)
     for i in range(M):
         for j in range(M):
-            cls_score_i = box_cls_scores[i].resize(19, 1)
-            cls_score_j = box_cls_scores[j]
+            cls_score_i = box_cls_scores[i].clone().detach().cpu().numpy().resize(19, 1)
+            cls_score_j = box_cls_scores[j].clone().detach().cpu().numpy()
 
-            cls_score_i = cls_score_i.clamp(1e-6, 1 - 1e-6)
-            cls_score_j = cls_score_j.clamp(1e-6, 1 - 1e-6)
+            cls_score_i = cls_score_i.clamp(1e-6, 1 - 1e-6).clone().detach().cpu().numpy()
+            cls_score_j = cls_score_j.clamp(1e-6, 1 - 1e-6).clone().detach().cpu().numpy()
 
-            loss -= sim_mat[i][j] * np.log(cls_score_i * cls_score_j) + (1 - sim_mat[i][j]) * np.log(1 - cls_score_i * cls_score_j)
+            loss -= sim_mat[i][j].clone().detach().cpu().numpy() * np.log(cls_score_i * cls_score_j) + (1 - sim_mat[i][j]) * np.log(1 - cls_score_i * cls_score_j)
 
     loss *= (1/M**2)
 
-    return loss.mean()
+    return loss
