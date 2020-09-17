9e5cadcc175c2811a95e08fb8284aa2ab37d7961
HEIMaxER <maxime.plocharski@gmail.com>
2020-09-17 14:53:58 +0200

    accelerating code

--- lib/modeling/vgg16.py
+++ lib/modeling/vgg16.py
@@ -192,7 +192,7 @@ class roi_2mlp_head_with_sim(nn.Module):
         x = F.relu(self.fc1(x.view(batch_size, -1)), inplace=True)
         x = F.relu(self.fc2(x), inplace=True)
 
-        feature_ranking = torch.from_numpy(np.argsort(x.clone().detach().numpy(), axis=1)).to('cuda')
+        feature_ranking = torch.from_numpy(np.argsort(x.clone().detach().cpu().numpy(), axis=1)).to('cuda')
         N = feature_ranking.shape[0] - 1
         if not self.strict_sim:
             for i in range(batch_size):
