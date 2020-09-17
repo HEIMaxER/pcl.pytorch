0bee315297c60ab64be14cdeddb870fbcea14e36
HEIMaxER <maxime.plocharski@gmail.com>
2020-09-17 16:28:11 +0200

    accelerating code

--- lib/modeling/vgg16.py
+++ lib/modeling/vgg16.py
@@ -228,7 +228,6 @@ def freeze_params(m):
 
 def PairEnum(x,mask=None):
     # Enumerate all pairs of feature in x
-    assert x.ndimension() == 2, 'Input dimension must be 2'
     x1 = x.repeat(x.size(0),1)
     x2 = x.repeat(1,x.size(0)).view(-1,x.size(1))
     if mask is not None:
