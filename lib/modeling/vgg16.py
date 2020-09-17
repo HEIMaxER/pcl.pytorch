e47b0659ca3293c85f12504dde94486bb0b8b59a
HEIMaxER <maxime.plocharski@gmail.com>
2020-09-17 14:51:35 +0200

    accelerating code

--- lib/modeling/vgg16.py
+++ lib/modeling/vgg16.py
@@ -192,19 +192,32 @@ class roi_2mlp_head_with_sim(nn.Module):
         x = F.relu(self.fc1(x.view(batch_size, -1)), inplace=True)
         x = F.relu(self.fc2(x), inplace=True)
 
-
-        feature_ranking = np.argsort(x.clone().detach().cpu().numpy(), axis=1)
-        N = feature_ranking.shape[0]-1
+        feature_ranking = torch.from_numpy(np.argsort(x.clone().detach().numpy(), axis=1)).to('cuda')
+        N = feature_ranking.shape[0] - 1
         if not self.strict_sim:
             for i in range(batch_size):
-                feature_ranking[i] = np.sort(feature_ranking[i][N-self.sim_dim:])  #if feature ranking doesn't have to be strictly equal, top k feature positions are sorted for easier checking
+                feature_ranking[i] = np.sort(feature_ranking[i][
+                                             N - self.sim_dim:])  # if feature ranking doesn't have to be strictly equal, top k feature positions are sorted for easier checking
 
         sim_mat = torch.zeros(batch_size, batch_size, device='cuda')
         for i in range(batch_size):
             for j in range(i, batch_size):
-                if (feature_ranking[i][N-self.sim_dim:] == feature_ranking[j][N-self.sim_dim:]).all():
+                if torch.eq(feature_ranking[i][N - self.sim_dim:], feature_ranking[j][N - self.sim_dim:]):
                     sim_mat[i][j] = 1
                     sim_mat[j][i] = 1
+
+        # feature_ranking = np.argsort(x.clone().detach().cpu()numpy(), axis=1)
+        # N = feature_ranking.shape[0]-1
+        # if not self.strict_sim:
+        #     for i in range(batch_size):
+        #         feature_ranking[i] = np.sort(feature_ranking[i][N-self.sim_dim:])  #if feature ranking doesn't have to be strictly equal, top k feature positions are sorted for easier checking
+        #
+        # sim_mat = torch.zeros(batch_size, batch_size, device='cuda')
+        # for i in range(batch_size):
+        #     for j in range(i, batch_size):
+        #         if (feature_ranking[i][N-self.sim_dim:] == feature_ranking[j][N-self.sim_dim:]).all():
+        #             sim_mat[i][j] = 1
+        #             sim_mat[j][i] = 1
         return x, sim_mat
 
 
