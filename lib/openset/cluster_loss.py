930031ee52542c3fce508f6dbcc18ad3bd955338
HEIMaxER <maxime.plocharski@gmail.com>
2020-09-23 21:12:44 +0200

    print

--- lib/openset/cluster_loss.py
+++ lib/openset/cluster_loss.py
@@ -11,11 +11,7 @@ import numpy as np
 import math
 
 def BCE_loss(box_cls_scores, sim_mat):
-    # box_cls_scores = box_cls_scores.clamp(1e-6, 1 - 1e-6)
-    for i in range(box_cls_scores.size(0)):
-        for j in range(box_cls_scores.size(1)):
-            if box_cls_scores[i][j] <= 0:
-                print('yes yes yes yes', box_cls_scores[i][j])
+    box_cls_scores = box_cls_scores.clamp(1e-6, 1 - 1e-6)
     t_box_cls_scores = torch.transpose(box_cls_scores, 0, 1)
     loss = - sim_mat * torch.log(torch.mm(box_cls_scores, t_box_cls_scores)) - (1 - sim_mat) * torch.log(1 - torch.mm(box_cls_scores, t_box_cls_scores))
     loss = loss.mean()
