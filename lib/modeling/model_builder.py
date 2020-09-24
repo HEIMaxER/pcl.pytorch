1a4a31496ee1e3330b5ef2bccf0f9e01624a0e0d
HEIMaxER <maxime.plocharski@gmail.com>
2020-09-24 10:28:41 +0200

    fixed stupidity

--- lib/modeling/model_builder.py
+++ lib/modeling/model_builder.py
@@ -278,9 +278,7 @@ class Sim_RCNN(nn.Module):
         if not self.training:
             return_dict['blob_conv'] = blob_conv
 
-        print('len roi befor', rois.shape)
         box_feat, sim_mat, rois = self.Box_Head(blob_conv, rois)
-        print('len roi after', rois.shape)
         mil_score = self.Box_MIL_Outs(box_feat)
         refine_score = self.Box_Refine_Outs(box_feat)
 
