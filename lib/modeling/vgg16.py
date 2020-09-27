62719e88871bb8af0f2376a018a9156128b85e14
HEIMaxER <maxime.plocharski@gmail.com>
2020-09-27 02:02:35 +0200

    print

--- lib/modeling/vgg16.py
+++ lib/modeling/vgg16.py
@@ -194,7 +194,6 @@ class roi_2mlp_head_with_sim(nn.Module):
         x = F.relu(self.fc2(x), inplace=True)
 
         if batch_size > 3000:
-            print('yes')
             _, feature_ranking = torch.sort(x, dim=1, descending=True)
             feature_ranking = feature_ranking[:, :self.sim_dim]
             feature_ranking, _ = torch.sort(feature_ranking, dim=1)
