305cb5533507de5957847bfb5b95fd13fbeee3d5
HEIMaxER <maxime.plocharski@gmail.com>
2020-10-03 15:09:16 +0200

    fix

--- lib/core/test.py
+++ lib/core/test.py
@@ -78,6 +78,8 @@ def im_detect_all(model, im, box_proposals=None, timers=None):
     # timers['misc_bbox'].tic()
     # scores, boxes, cls_boxes = box_results_with_nms_and_limit(scores, boxes)
     # timers['misc_bbox'].toc()
+
+    print(sim_mat)
     if sim_mat != None:
         return {'scores': scores, 'boxes' : boxes, 'sim_mat': sim_mat}
 
